81dfcdd3162388e8a11394272137e754652f549e
sochetanie <allagorik2901@gmail.com>
2017-06-09 10:58:07 -0400

    done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt () {
+    $('p').on('click', function () {
+        alert('Hey!')
+    })
+}
 
+function frameIt () {
+    $('img').load(function () {
+        $('img').addClass('tasty')
+    })
+}
+
+function pressIt () {
+    $('#typing').on('keydown', function (e) {
+        if (e.which == "71") {
+        alert('G have been pressed')
+    }
+    })
+}
+
+function submitIt () {
+    $('form').on('submit', function () {
+        alert('Your form is going to be submitted now.')
+    })
+}
+
+$(document).ready(function(){
 // call functions here
+
+    getIt()
+    frameIt()
+    pressIt()
+    submitIt()
 
 });
